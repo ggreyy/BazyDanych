2111d9544dddce06b8e34a81b933fb3adeb0fda2
ggreyy <greg.dzieciol@gmail.com>
2021-03-17 16:01:08 +0100

    Update 3

--- skryptnabd.ps1
+++ skryptnabd.ps1
@@ -1,21 +1,5 @@
-﻿
-
-
-echo "[*]Witam w programie frontendowym bazy danych! Upewnij sie ze masz uprawnienia administratora w tym oknie"
+﻿echo "[*] Witaj! Upewnij sie ze masz uprawnienia administratora aby zainstalowac ta zaleznosc: Simply-SQL"
 Pause
-echo "[*]Aby program dzialal poprawnie musimy pobrac pewne zaleznosci oraz ustawic PolitykeWykonywania na 'YES'"
-Pause
-echo "[*]Zatwierdz ta opcje(pozniej mozna sobie ja zmienic, to tylko po to zeby zainstalowac zaleznosc"
-Pause
-Set-ExecutionPolicy RemoteSigned
-$location = Read-host -Prompt "Wpisz sciezke instalacji dla modulu Powershella"
-$location = -join($location, "\", "MySql.zip")
-Invoke-WebRequest  -Uri https://github.com/adbertram/MySQL/archive/master.zip -OutFile  ( New-Item -Path "$location" -Force )
-$modulesFolder =  'C:\Program Files\WindowsPowerShell\Modules'
-Expand-Archive -Path  "$location" -DestinationPath $modulesFolder
-Rename-Item -Path  "$modulesFolder\MySql-master" -NewName MySQL
-echo "[*]Teraz podaj dane logowania do bazy danych"
-$dbCred =  Get-Credential
-
-Pause
-
+#Install-Module -Name SimplySql -RequiredVersion 1.6.2
+Open-MySqlConnection -DataSource "localhost" -InitialCatalog "baza_danych"
+Show-SqlConnection
