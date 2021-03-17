85f3f6c02fe8aaa690765c15752788db542eaffb
ggreyy <greg.dzieciol@gmail.com>
2021-03-17 16:41:53 +0100

    update nastepny

--- skryptnabd.ps1
+++ skryptnabd.ps1
@@ -4,3 +4,7 @@
 echo "[*] Uruchom ten program w trybie administratora, zeby zainstalowac zaleznosci. Jesli tego nie zrobiles to wyjdz CTRL + C"
 Pause
 Install-Module -Name SimplySql -RequiredVersion 1.6.2 -Scope CurrentUser -verbose
+echo "[*] Zaleznosc zainstalowana pomyslnie! Teraz podaj nazwe uzytkownika bazy danych oraz jego haslo (puste haslo jest zabronione)"
+$login = Read-Host -Prompt 'Podaj login'
+$haslo = Read-Host -Prompt 'Podaj haslo'
+Open-MySqlConnection -Server "localhost" -UserName $login -Password $haslo
