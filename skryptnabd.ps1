8ef063f9cde27c5bc636410595d6882fde6f4575
ggreyy <greg.dzieciol@gmail.com>
2021-03-19 20:11:07 +0100

    sql queries updated

--- skryptnabd.ps1
+++ skryptnabd.ps1
@@ -21,13 +21,16 @@ $haslo = Read-Host -Prompt 'Podaj haslo'
 Open-MySqlConnection -Server "localhost" -UserName $login -Password $haslo
 
 
+#wypelnij beznagrod
 
 Invoke-SqlQuery -query "CREATE DATABASE wystawa_psow_rasowych;USE wystawa_psow_rasowych;"
 Invoke-SqlQuery -query "CREATE TABLE Kategorie (id_kat int, kat varchar(20), rasa varchar(20), wartosc_nagr int, id_psa int,PRIMARY KEY (id_kat) );"
 Invoke-SqlQuery -query "CREATE TABLE Wlasciciele (id_wlasc int, id_psa int, imie varchar(20), miasto varchar(20),nazwisko varchar(20), imie_psa varchar(20), PRIMARY KEY (id_wlasc));"
 Invoke-SqlQuery -query "CREATE TABLE Psy (id_psa int, imie_psa varchar(20),wiek int, rasa varchar(20),plec varchar(1),id_wlasc int, PRIMARY KEY (id_psa));"
-Invoke-SqlQuery -query "CREATE TABLE Nagrody (id_nagr int, imie_psa varchar(20),wartosc_nagr int, rasa varchar(20),id_psa int, FOREIGN KEY (id_psa) REFERENCES Psy (id_psa));"
+Invoke-SqlQuery -query "CREATE TABLE Nagrody (id_nagr int, imie_psa varchar(20),wartosc_nagr int, rasa varchar(20),id_psa int, FOREIGN KEY (id_psa) REFERENCES Psy (id_psa), PRIMARY KEY (id_psa,id_nagr));"
 Invoke-SqlQuery -query "CREATE TABLE Sprzedaz (id_psa int, cena int,FOREIGN KEY (id_psa) REFERENCES Psy (id_psa), PRIMARY KEY (id_psa));"
+Invoke-SqlQuery -query "CREATE TABLE Sedzia (id_psa int, wartosc_nagr int,FOREIGN KEY (id_psa) REFERENCES Psy(id_psa), PRIMARY KEY (id_psa));"
 
+ 
 
 Close-SqlConnection
